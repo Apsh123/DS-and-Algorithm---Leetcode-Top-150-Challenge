afa17ed3eed32a64f1d50a2ac28fa095a0d7b053
Apurva Shrivastava <apurva.shrivastava@tamu.edu>
2018-01-05 21:47:20 -0600

    1.java

---
+++ 1.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+         if(nums.length>=2)
+        {
+            HashMap<Integer,Integer> h = new HashMap<Integer,Integer>();
+            for(int i=0; i<nums.length; i++){
+                if(!h.isEmpty() && h.containsKey(target-nums[i]))
+                {
+                    int[] k = {h.get(target-nums[i]),i};
+                    return k;
+                }
+                
+                h.put(nums[i],i);
+            }
+        }
+        
+        int[] k = {-1,-1};
+        return k;
+    }
+}
